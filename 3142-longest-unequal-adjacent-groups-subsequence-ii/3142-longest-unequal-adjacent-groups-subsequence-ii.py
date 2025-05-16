a60bc46b2c3ea7ac8e6da53452be26899dcd39d6
sama umesh reddy <152951237+umesh755485@users.noreply.github.com>
2025-05-16 05:55:53 +0530

    Time: 1063 ms (48.86%), Space: 18.1 MB (75%) - LeetHub

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.py
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.py
@@ -1,31 +1,32 @@
-from typing import List
+class Solution(object):
+    def differByOneChar(self, word1, word2):
+        if len(word1) != len(word2):
+            return False
+        diffCount = 0
+        for c1, c2 in zip(word1, word2):
+            if c1 != c2:
+                diffCount += 1
+        return diffCount == 1
 
-class Solution:
-    def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
-        n = len(words)
-        
-        def hamming_distance(w1, w2):
-            return sum(a != b for a, b in zip(w1, w2))
-        
-        dp = [1] * n 
-        prev = [-1] * n 
-        
+    def getWordsInLongestSubsequence(self, words, groups):
+        n = len(groups)
+        dp = [1] * n
+        parent = [-1] * n
+        maxi = 0
         for i in range(n):
             for j in range(i):
-                if (groups[i] != groups[j] and
-                    len(words[i]) == len(words[j]) and
-                    hamming_distance(words[i], words[j]) == 1):
-                    if dp[j] + 1 > dp[i]:
-                        dp[i] = dp[j] + 1
-                        prev[i] = j
-        
-        max_len = max(dp)
-        idx = dp.index(max_len)
-        
-        res_indices = []
-        while idx != -1:
-            res_indices.append(idx)
-            idx = prev[idx]
-        res_indices.reverse()
-        
-        return [words[i] for i in res_indices]
+                if groups[i] != groups[j] and \
+                   self.differByOneChar(words[i], words[j]) and \
+                   dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    parent[i] = j
+            if dp[i] > maxi:
+                maxi = dp[i]
+        result = []
+        for i in range(n):
+            if dp[i] == maxi:
+                while i != -1:
+                    result.append(words[i])
+                    i = parent[i]
+                break
+        return result[::-1]
